221d407c79c328686c7c3f147e824208d65f7d73
Pradeep Elango <pradeep1991@gmail.com>
2019-05-04 16:23:23 +0800

    Added hibernate and db script for account

--- account-service/src/main/java/com/study/pattern/account/model/Account.java
+++ account-service/src/main/java/com/study/pattern/account/model/Account.java
@@ -1,42 +1,55 @@
 package com.study.pattern.account.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="account")
 public class Account {
 
+	@Column(name="id")
 	private Integer id;
+
+	@Column(name="customer_id")
 	private Integer customerId;
-	private String number;
+
+	@Column(name="account_number")
+	private String accountNumber;
+
+	@Column(name="account_type")
+	private String accountType;
+
+	@Column(name="date_created")
+	private Date dateCreated;
+
+	@Column(name="date_modified")
+	private Date dateModified;
 
 	public Account() {
 
 	}
 
-	public Account(Integer id, Integer customerId, String number) {
+	public Account(Integer id, Integer customerId, String accountNumber, String accountType, Date dateCreated,
+			Date dateModified) {
+		super();
 		this.id = id;
 		this.customerId = customerId;
-		this.number = number;
+		this.accountNumber = accountNumber;
+		this.accountType = accountType;
+		this.dateCreated = dateCreated;
+		this.dateModified = dateModified;
 	}
 
-	public Integer getId() {
-		return id;
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", customerId=" + customerId + ", accountNumber=" + accountNumber + ", accountType="
+				+ accountType + ", dateCreated=" + dateCreated + ", dateModified=" + dateModified + "]";
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
 }
--- account-service/src/main/java/com/study/pattern/account/repository/AccountRepository.java
+++ account-service/src/main/java/com/study/pattern/account/repository/AccountRepository.java
@@ -2,10 +2,16 @@ package com.study.pattern.account.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.study.pattern.account.model.Account;
 
-public interface AccountRepository  {
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
-    public Account findByNumber(String number);
+    public Account findByAccountNumber(String accountNumber);
+
     public List<Account> findByCustomerId(String customerId);
+
 }
